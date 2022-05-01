b9d39632169e4588ba57a081d093be9ad1d88946
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-05-01 21:02:29 +0300

    tasc5

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -13,13 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater1(options ) {
+ let str =''
+ let addition
+ if(typeof options.addition === "undefined"){
+  addition = ''
+ } else{
+   addition = '' + options.addition 
+ }
+ 
+ let countRepeat =  options.additionRepeatTimes || 1
+ let separator = options.additionSeparator || "|"
+for(let i= 1; i<countRepeat; i++){
+  str += addition +separator
+}
+str += addition
+
+return str
 }
 
+function repeater(str1, options) {
+  if(!options){
+    return str1
+  }
+  let str = '' + str1
+ let res = ''
+ let countRepeat = options.repeatTimes || 1
+let separator = options.separator || "+"
+let addition = repeater1(options)
+for (let i = 1; i<countRepeat; i++){
+  res += str + addition + separator
+}
+res += str + addition
+return res
+
+}
+; 
 module.exports = {
   repeater
 };
+repeater(repeater('la', { repeatTimes: 3 }))
+//, 'la+la+la');
